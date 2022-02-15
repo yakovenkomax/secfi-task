b2c2a6886376f049dbded8a5e42f22873b8cd677
yakovenkomax <yakovenkomax@outlook.com>
2022-02-15 21:31:51 +0100

    Add Select component

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,34 @@
+import { ChangeEventHandler } from 'react';
+
+export type SelectItem = {
+  label: string;
+  value: string;
+};
+
+export type SelectProps = {
+  items: SelectItem[];
+  value?: string;
+  placeholder?: string;
+  onChange?: ChangeEventHandler<HTMLSelectElement>;
+};
+
+const EMPTY_VALUE = '';
+
+export const Select = (props: SelectProps) => {
+  const { items, value = EMPTY_VALUE, placeholder, onChange } = props;
+
+  return (
+    <select value={value} onChange={onChange}>
+      {placeholder && (
+        <option key="placeholder" value={EMPTY_VALUE}>
+          {placeholder}
+        </option>
+      )}
+      {items.map(item => (
+        <option key={item.value} value={item.value} selected={item.value === value}>
+          {item.label}
+        </option>
+      ))}
+    </select>
+  );
+};
